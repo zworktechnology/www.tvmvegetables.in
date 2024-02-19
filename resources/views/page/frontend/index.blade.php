124a2d3a0b8de212296294319eb075f3a03af9b2
Rajamurugan <rajamurugan6699@gmail.com>
2024-02-19 18:38:12 +0530

    Front End- Ui

--- resources/views/page/frontend/index.blade.php
+++ resources/views/page/frontend/index.blade.php
@@ -1,16 +1,88 @@
 </html>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Thirupathi Trading Co., English Vegetable Commission Agent</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    <title>TVM English Vegetable Commission Agent, Gandhi Market, Tiruchirappalli, Tamil Nadu, India - POS</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous">
+    </script>
     <style>
-        body{
-            background-image:url("assets/frontend/images/1.jpeg");
+        @import url('https://fonts.googleapis.com/css?family=Oswald:400,700');
+
+        body,
+        .centered-top-container.sticky {}
+
+        .news-container {
+            position: fixed;
+            bottom: 0;
+            right: 0;
+            left: 0;
+            font-size: 16px;
+            background: #C8102E;
+            color: #fff000;
+            text-shadow: 3px 3px #333;
+            padding: 10px 32px;
+            display: flex;
+            box-shadow: 0 4px 40px -8px rgb(0, 51, 98);
+            overflow: hidden;
+            font-family: 'Oswald', serif;
+            z-index: 500;
+            word-break: normal
+        }
+
+        .news-container::after {
+            position: absolute;
+            content: &quot;
+            &quot;
+            ;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            height: 10px;
+            background: linear-gradient(transparent, #003262);
+        }
+
+        .news-container .news-heading {
+            font-weight: bold;
+            padding-right: 30px;
+            margin-right: 30px;
+            border-right: 1px solid #fff;
+        }
+
+        .news-container .news {
+            position: relative;
+            width: 100%;
+        }
+
+        .news-container .news-single {
+            color: #fff;
+            text-decoration: none;
+            font-weight: normal;
+            opacity: 0;
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            transform: translateY(-8px);
+            transition: all 1000ms ease;
+            pointer-events: none;
+        }
+
+        .news-container .news-single.active {
+            opacity: 1;
+            transform: translateY(0);
+            pointer-events: auto;
+        }
+
+        body {
+            background-image: url("assets/frontend/images/1.jpg");
             font-family: Georgia, serif;
             background-repeat: no-repeat;
             background-position: center;
@@ -18,15 +90,34 @@
             background-size: cover;
             height: 100%;
         }
-        img{
-             display: block;
-             margin-left: auto;
-             margin-right: auto;
-             padding: 90px 0px 0px 0px;
+
+        img {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            padding: 90px 0px 0px 0px;
         }
     </style>
 </head>
+
 <body>
-        <img src="{{ asset('assets/frontend/images/2.png') }}">
+    {{-- <img src="{{ asset('assets/frontend/images/2.png') }}"> --}}
 </body>
+<div class="news-container">
+    <div class="news-heading" style="width: 110px;">Crafted By</div>
+
+    <div class="news">
+        <a href="https://zworktechnology.com/" class="news-single active">
+            www.zworktechnology.com
+        </a>
+
+        <a href="https://maps.app.goo.gl/6k17Gngd5QBUA2bF8" class="news-single">
+            LIC Colony, Tiruchirappalli, Tamil Nadu 620021
+        </a>
+    </div>
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/gh/livebloggerofficial/News-Ticker@main/main.js"></script>
+
 </html>
